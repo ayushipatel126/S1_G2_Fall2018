0f68aa751575e5a28df49e724ccc6d45ad6e61d9
Ayushi Patel <ayushipatel126@gmail.com>
2018-09-29 18:49:55 -0400

    First commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!E:\Masters\Sem 3\Internet Applications and Distributed Systems\Labs\Lab 3\S1_G2_Fall2018\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
